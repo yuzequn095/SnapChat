fc8fab779c513c0e9adb4b8e2e4e93073481e810
ZEQUN YU <yuzequn095@gmail.com>
2021-02-19 16:50:19 -0800

    allow user to choose photos from external resource, and fix default pic display bugs

--- app/src/main/java/com/example/snapchat/CreateSnapActivity.kt
+++ app/src/main/java/com/example/snapchat/CreateSnapActivity.kt
@@ -1,5 +1,6 @@
 package com.example.snapchat
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import java.lang.Exception
 import java.util.jar.Manifest
 
 class CreateSnapActivity : AppCompatActivity() {
@@ -30,11 +32,42 @@ class CreateSnapActivity : AppCompatActivity() {
 
     // import something
     fun chooseImageClicked(view: View){
+        // If we don't got the permission to choose photos from outside
         if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1) // request the permission
         }
         else{
             getPhoto()
         }
     }
+
+    // get the result from the previous activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val selectedImage = data!!.data // get the return image from previous activity
+
+        // if return data is valid
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK && data != null){
+            try{
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                createSnapImageView?.setImageBitmap(bitmap)
+            }
+            catch(e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Callback for the result from requesting permissions.
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getPhoto()
+            }
+        }
+    }
+
 }
